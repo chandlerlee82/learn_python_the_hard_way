71799922389a89bacb68f35334be0a4282cc603b
chandlerlee82 <chandlerlee82@gmail.com>
2013-09-08 17:14:23 +0800

    add ex3_float

---
+++ ex3_float.py
@@ -0,0 +1,28 @@
+print "I will now count my chickens:"
+
+print "Hens", 25 + 30.4 / 6
+
+#25.5 * 3 % 4=76.5%4=0.5
+print "Roosters", 100.2 - 25.5 * 3 % 4
+
+print "Now I will count the eggs:"
+
+print 3.2 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+print "Is it true that 3 + 2 < 5 - 7?"
+
+print 3 + 2 < 5 - 7
+
+print "What is 3 + 2?", 3 + 2
+print "What is 5 - 7?", 5 - 7
+
+print "Oh, that's why it's False."
+
+print "How about some more."
+
+print "Is it greater?", 5 > -2
+print "Is it greater or equal?", 5 >= -2
+print "Is it less or equal?", 5 <= -2
+
+print "Is it equal?",5==5.00000
+print "Is it equal?",5==5.0000001
